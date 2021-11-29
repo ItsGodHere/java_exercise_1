f648c39f26cbebe01a68490e1a1beeb0bb57f5eb
Shrafdine OURO-AGORO <shouro-agoro@et.esiea.fr>
2021-11-29 15:38:31 +0100

    Partie 3 - boucle

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Launcher {
+public class Launcher {
+    public static void main(String[] args) {
+        java.util.Scanner entree =   new java.util.Scanner(System.in);
+        System.out.println("Welcome!!");
+        String phrase = entree.nextLine();
+        while(!phrase.equals("quit")) {
+            System.out.println("Unknown command");
+            phrase = entree.nextLine();
+        }
+    }
 }
