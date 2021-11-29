cff121ded628f097c53138a6f531f88132060e99
Shrafdine OURO-AGORO <shouro-agoro@et.esiea.fr>
2021-11-29 16:15:00 +0100

    Partie 4 - une première commande

--- src/Launcher.java
+++ src/Launcher.java
@@ -2,10 +2,29 @@ public class Launcher {
     public static void main(String[] args) {
         java.util.Scanner entree =   new java.util.Scanner(System.in);
         System.out.println("Welcome!!");
-        String phrase = entree.nextLine();
+        int nb = 0;
+        int fibo = 0;
+        String phrase = "";
         while(!phrase.equals("quit")) {
-            System.out.println("Unknown command");
-            phrase = entree.nextLine();
+            if (phrase.equals("")){
+                phrase = entree.nextLine();
+            }else {
+                if (phrase.equals("fibo")) {
+                    System.out.println("Enter a number:");
+                    nb = entree.nextInt();
+                    entree.nextLine();
+                    fibo = fib(nb);
+                    System.out.println(fibo);
+                    phrase = entree.nextLine();
+                }else {
+                    System.out.println("Unknown command");
+                    phrase = entree.nextLine();
+                }
+            }
         }
+    }
+    private static int fib(int n) {
+        if (n <= 1) return n;
+        else return fib(n-1) + fib(n-2);
     }
 }
